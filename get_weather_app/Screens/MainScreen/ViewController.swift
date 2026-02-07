7673db504e32616d646ee7fc6e61e9bbbe7af80b
Albert Gimalov <gimalov@kolesa.kz>
2026-02-07 20:02:23 +0500

    create base architacture

---
+++ get_weather_app/Screens/MainScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  get_weather_app
+//
+//  Created by Albert Gimalov on 07.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ get_weather_app/get_weather_appTests/get_weather_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  get_weather_appTests.swift
+//  get_weather_appTests
+//
+//  Created by Albert Gimalov on 07.02.2026.
+//
+
+import Testing
+@testable import get_weather_app
+
+struct get_weather_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
